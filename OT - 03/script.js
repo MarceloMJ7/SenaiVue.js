19994d1322c91ec2b155fd0fe79352402ac29eff
Marcelo <marcelo_mj7@hotmail.com>
2025-07-20 20:12:28 -0300

    continuando Vue

---
+++ OT - 03/script.js
@@ -0,0 +1,12 @@
+// Correto
+var app = new Vue({
+  // Não esqueça do "new"
+  el: "#app",
+  data: {
+    // A palavra é "data" e ela contém um objeto
+    titulo: "Formulário Básico", // A variável "titulo" vai aqui dentro
+    nome: "",
+    email: "",
+    telefone: "",
+  },
+});
